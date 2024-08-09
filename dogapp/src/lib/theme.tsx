bf5211e9ce49333e4232e0c6bfbb41ec025c9ed3
cynthia-isutsa <isutsacynthia@gmail.com>
2024-08-09 05:32:45 +0300

    feat: [new structure using yarn && styled components]

---
+++ dogapp/src/lib/theme.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle
+
+const theme = () => {
+  return (
+    <div>Theme</div>
+  )
+}
+
+export default theme
